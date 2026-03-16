ee4dae441d741d4f7c23036f71a7e742e9fb59da
Raj Sahu <55652441+rajsahu-git@users.noreply.github.com>
2026-03-16 14:26:05 +0530

    added intro on template

--- src/template/marriage/template-4/App.tsx
+++ src/template/marriage/template-4/App.tsx
@@ -1,4 +1,5 @@
 // import React from 'react';
+// import { useState } from 'react';
 import { Hero } from './components/Hero';
 // import { CoupleIntro } from './components/CoupleIntro';
 import { Countdown } from './components/Countdown';
@@ -6,7 +7,18 @@ import { Timeline } from './components/Timeline';
 import { Gallery } from './components/Gallery';
 import { RSVPForm } from './components/RSVPForm';
 import { Footer } from './components/Footer';
+// import { VideoIntro } from './components/VideoIntro';
 export function App() {
+  // const [showIntro, setShowIntro] = useState(true);
+
+  // const handleVideoEnd = () => {
+  //   setShowIntro(false);
+  // };
+
+  // if (showIntro) {
+  //   return <VideoIntro onVideoEnd={handleVideoEnd} />;
+  // }
+
   return (
     <div className="min-h-screen bg-cream">
       <Hero />
